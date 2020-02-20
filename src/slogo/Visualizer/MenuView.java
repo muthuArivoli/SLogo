5e1f1c833a30fa8f6b113f1177940519ff596256
Rodrigo <rea20@duke.edu>
2020-02-20 02:54:56 -0500

    Set up basic Front-End model, with three different Panes: Grid, Menu, and Script. These will independtly interact with the backend or the interface based on their roles. Next steps will be adding functionality to these classes.

--- src/slogo/Visualizer/MenuView.java
+++ src/slogo/Visualizer/MenuView.java
@@ -1,14 +1,47 @@
 package slogo.Visualizer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MenuView extends Pane {
 
     private static final double VIEWWIDTH = 10;
     private static final double VIEWHEIGHT = 10;
+    private HBox menuPane;
 
-    public MenuView(Stage primaryStage) {
+    public MenuView() {
+        menuPane = new HBox();
 
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "Chinese",
+                        "English",
+                        "French",
+                        "German",
+                        "Italian",
+                        "Portuguese",
+                        "Russian",
+                        "Spanish",
+                        "Urdu"
+                );
+
+        ComboBox left = new ComboBox(options);
+        Pane spacer = new Pane();
+        Button right = new Button("Run");
+
+        left.setPromptText("Languages");
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        spacer.setMinSize(10, 0);
+        menuPane.getChildren().addAll(left, spacer, right);
     }
+
+    public HBox getPane() {return menuPane;}
 }
--- src/slogo/Visualizer/ScriptView.java
+++ src/slogo/Visualizer/ScriptView.java
@@ -1,14 +1,22 @@
 package slogo.Visualizer;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+
 
 public class ScriptView extends Pane {
 
     private static final double VIEWWIDTH = 10;
     private static final double VIEWHEIGHT = 10;
+    private VBox scriptPane;
 
-    public ScriptView(Stage primaryStage) {
-
+    public ScriptView() {
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(200, 600);
+        scriptPane = new VBox(textArea);
     }
+
+    public VBox getPane() {return scriptPane;}
 }
