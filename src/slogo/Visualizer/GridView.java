be935c09d19b1bb59af341d6a237c58a80920c34
Rodrigo <rea20@duke.edu>
2020-02-24 15:09:11 -0500

    made text script area resizeable vertically

--- src/slogo/Visualizer/GridView.java
+++ src/slogo/Visualizer/GridView.java
@@ -24,9 +24,8 @@ public class GridView extends Pane {
         gridPane.setPadding(new Insets(0, 0, 0, 0));
         turtleSource = new Image("turtle.png");
         turtle = resizeImage(turtleSource);
-        turtle = centerImage(turtle);
         gridSizer(numCols, numRows);
-        gridPane.add(turtle, numCols/2, numRows/2);
+        gridPane.add(turtle, 571, 571);
         GridPane.setHalignment(turtle, HPos.CENTER);
         GridPane.setValignment(turtle, VPos.CENTER);
 
@@ -44,39 +43,17 @@ public class GridView extends Pane {
         return result;
     }
 
-    public ImageView centerImage(ImageView input) {
-        Image img = input.getImage();
-        if (img != null) {
-            double w = 0;
-            double h = 0;
-
-            double ratioX = input.getFitWidth() / img.getWidth();
-            double ratioY = input.getFitHeight() / img.getHeight();
-
-            double reducCoeff = 0;
-            if(ratioX >= ratioY) {
-                reducCoeff = ratioY;
-            } else {
-                reducCoeff = ratioX;
-            }
-
-            w = img.getWidth() * reducCoeff;
-            h = img.getHeight() * reducCoeff;
-
-            input.setX((input.getFitWidth() - w) / 2);
-            input.setY((input.getFitHeight() - h) / 2);
-        }
-        return input;
-    }
-
     private void gridSizer(int cols, int rows) {
         for (int i = 0; i < cols; i++) {
             ColumnConstraints colConst = new ColumnConstraints(1);
             gridPane.getColumnConstraints().add(colConst);
+            colConst.setHgrow(Priority.NEVER);
+
         }
         for (int i = 0; i < rows; i++) {
             RowConstraints rowConst = new RowConstraints(1);
             gridPane.getRowConstraints().add(rowConst);
+            rowConst.setVgrow(Priority.NEVER);
         }
     }
 }
