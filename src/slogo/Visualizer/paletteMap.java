e4c0adee1728e335473cd13a77c9d74c6db4d4e8
Rodrigo <rea20@duke.edu>
2020-03-03 16:33:57 -0500

    reformated palette viewer. Users can now copy web string values

--- src/slogo/Visualizer/paletteMap.java
+++ src/slogo/Visualizer/paletteMap.java
@@ -2,6 +2,8 @@ package slogo.Visualizer;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -28,27 +30,35 @@ public class paletteMap {
     public HashMap<Integer, Color> getColorMap() {return colorMap;}
 
     public HBox createScene(HBox input) {
-        VBox indexCol = new VBox(5);
+        VBox indexCol = new VBox(10);
         indexCol.setPadding(new Insets(10,10,10,30));
         VBox stringCol = new VBox(5);
         stringCol.setPadding(new Insets(10,10,10,10));
-        VBox colorCol = new VBox(6.5);
+        VBox colorCol = new VBox(10);
         colorCol.setPadding(new Insets(10,10,10,10));
+        VBox indexCol2 = new VBox(10);
+        VBox stringCol2 = new VBox();
+        VBox colorCol2 = new VBox(12);
         Label in = new Label("Index:");
-        Label s = new Label("String Value:");
+        Label s = new Label("Web Value:");
         Label c = new Label("Color:");
         indexCol.getChildren().add(in);
         stringCol.getChildren().add(s);
         colorCol.getChildren().add(c);
         for (int i = 0; i < colorMap.size(); i++) {
             Label num = new Label(Integer.toString(i));
-            Label col = new Label(colorMap.get(i).toString());
+            TextField col = new TextField(colorMap.get(i).toString().substring(2, 8));
+            col.setEditable(false);
+            col.setMaxWidth(100);
             Rectangle r = new Rectangle(15, 15, colorMap.get(i));
 
-            indexCol.getChildren().add(num);
-            stringCol.getChildren().add(col);
-            colorCol.getChildren().add(r);
+            indexCol2.getChildren().add(num);
+            stringCol2.getChildren().add(col);
+            colorCol2.getChildren().add(r);
         }
+        indexCol.getChildren().add(indexCol2);
+        stringCol.getChildren().add(stringCol2);
+        colorCol.getChildren().add(colorCol2);
         input.getChildren().addAll(indexCol, colorCol, stringCol);
         return input;
     }
